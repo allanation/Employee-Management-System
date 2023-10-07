25654468d7cbcd91f56ee80aedb1f306419bf547
allan <48453731+allanation@users.noreply.github.com>
2023-10-07 17:49:50 -0400

    EMS-1 Defined and created database structures #1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,135 @@
 #include <iostream>
+#include <fstream>
+#include <map>
 
 using namespace std;
+
+// Define the Employee class for more complex behavior and encapsulation
+class Employee
+{
+private:
+    char firstName[50], lastName[50], jobTitle[50];
+    int id;
+
+public:
+    // Constructor for the Employee class
+    Employee(int _id, const char *_firstName, const char *_lastName, const char *_jobTitle)
+        : id(_id)
+    {
+        strncpy(firstName, _firstName, sizeof(firstName));
+        strncpy(jobTitle, _lastName, sizeof(lastName));
+        strncpy(jobTitle, _jobTitle, sizeof(jobTitle));
+    }
+
+    void createEmployee()
+    {
+    }
+    void readEmployees()
+    {
+    }
+    void updateEmployee()
+    {
+    }
+    void deleteEmployee()
+    {
+    }
+};
+
+// Define the Department class for more complex behavior and encapsulation
+class Department
+{
+private:
+    char name[50], managedBy[50];
+    int id, officeId, companyId;
+
+public:
+    // Constructor for the Department class
+    Department(int _id, int _officeId, int _companyId, char *_name, char *_managedBy)
+        : id(_id), officeId(_officeId), companyId(_companyId)
+    {
+        strncpy(name, _name, sizeof(name));
+        strncpy(managedBy, _managedBy, sizeof(managedBy));
+    }
+
+    void createDepartment()
+    {
+    }
+    void readDepartments()
+    {
+    }
+    void updateDepartment()
+    {
+    }
+    void deleteDepartment()
+    {
+    }
+};
+
+class Office
+{
+private:
+    char location[50];
+    int id, companyId;
+
+public:
+    Office(int _id, int _companyId, char *_location)
+        : id(_id), companyId(_companyId)
+    {
+        strncpy(location, _location, sizeof(location));
+    };
+
+    void createOffice()
+    {
+    }
+    void readOffices()
+    {
+    }
+    void updateOffice()
+    {
+    }
+    void deleteOffice()
+    {
+    }
+};
+
+class Company
+{
+private:
+    char name[50];
+    int id;
+
+public:
+    // Constructor for the Company class
+    Company(int _id, char *_name) : id(_id)
+    {
+        strncpy(name, _name, sizeof(name));
+    }
+
+    void createCompany()
+    {
+    }
+    void readCompanies()
+    {
+    }
+    void updateCompany()
+    {
+    }
+    void deleteCompany()
+    {
+    }
+};
+
 int main()
 {
     system("clear");
     cout
         << "\n\t\t\t* WELCOME TO THE COMPANY DATABASE *" << endl;
     cout << "\nDescription: Design a simple command-line database where you use pointers to manage records, tables, and data relationships. You can implement basic database operations like insertion, retrieval, and deletion.\n";
+
+    map<int, Employee> employees;
+    map<int, Department> departments;
+    map<int, Office> offices;
+    map<int, Company> companies;
+
     return 0;
 }
