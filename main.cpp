a47e98cb2399e8781914f363af227a7321b00906
allan <48453731+allanation@users.noreply.github.com>
2023-10-09 00:10:32 -0400

    EMS-2 Create function work! #2 #14

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ public:
     // Constructor for the Employee class
     Employee(int _id, int _departmentId, const string &_firstName, const string &_lastName, const string &_jobTitle)
     {
+        // OPTIMIZATION: KEEP TRACK OF ID TO AUTOMATICALLY ASSIGN ID WITHOUT CHECKING FOR DUPS
         id = _id;
         departmentId = _departmentId;
         firstName = _firstName;
@@ -71,6 +72,7 @@ public:
     // Constructor for the Department class$
     Department(int _id, int _companyId, int _managedBy, const string &_name, const string &_location)
     {
+        // OPTIMIZATION: KEEP TRACK OF ID TO AUTOMATICALLY ASSIGN ID WITHOUT CHECKING FOR DUPS
         id = _id;
         companyId = _companyId;
         managedBy = _managedBy;
@@ -121,15 +123,9 @@ public:
     // Constructor for the Company class
     Company(int _id, const string &_name)
     {
+        // OPTIMIZATION: KEEP TRACK OF ID TO AUTOMATICALLY ASSIGN ID WITHOUT CHECKING FOR DUPS
         id = _id;
         name = _name;
-    }
-
-    // TO BE REMOVED
-    Company()
-    {
-        id = 0;
-        name = "_name";
     }
 
     void createCompany()
@@ -153,6 +149,14 @@ public:
     }
 };
 
+struct Database
+{
+    // Initialize tables
+    map<int, Employee> employeesD;
+    map<int, Department> departmentsD;
+    map<int, Company> companiesD;
+};
+
 string *toUppercase(const string &_word)
 {
     string uppercase;
@@ -165,14 +169,14 @@ string *toUppercase(const string &_word)
     return pointer;
 }
 
-string *processString(const string &_string)
+string *processString(string &_string)
 {
-    string word;
 
     if (_string.find_first_of('"') == 0 && _string.find_last_of('"'))
     {
-        cout << "It's a string!" << endl;
-        string *pointer = new string(word);
+        _string.erase(0, 1);
+        _string.erase(_string.find_last_not_of('"') + 1);
+        string *pointer = new string(_string);
         return pointer;
     }
     else
@@ -189,7 +193,6 @@ int processInt(string &_int)
     try
     {
         num = std::stoi(_int);
-        cout << "The integer is: " << num << std::endl;
         return num;
     }
     catch (const invalid_argument &e)
@@ -203,6 +206,17 @@ int processInt(string &_int)
         exit(0);
     }
 }
+
+// OPTIMIZATION: MAKE FACTORY METHOD FOR TABLES
+// check this: https://www.geeksforgeeks.org/factory-method-for-designing-pattern/
+
+// Initialize tables
+map<int, Employee> employees;
+map<int, Department> departments;
+map<int, Company> companies;
+
+// Initialize database
+// Database db;
 
 void create(int tableSelected, string &_values)
 {
@@ -220,7 +234,8 @@ void create(int tableSelected, string &_values)
 
         deconstructedQuery.push_back(query);
     }
-    // This is where i set _values to the attributes
+
+    // Formatting deconstructedQuery to be validated
     for (int i = 0; i < deconstructedQuery.size(); i++)
     {
         if (i == 0)
@@ -233,31 +248,84 @@ void create(int tableSelected, string &_values)
             deconstructedQuery[i].erase(deconstructedQuery[i].find_last_not_of(";") + 1);
             deconstructedQuery[i].erase(deconstructedQuery[i].find_last_not_of(")") + 1);
         }
-
-        if (i == 0)
-        {
-            int check = processInt(deconstructedQuery[i]);
-        }
-        if (i == 1)
-        {
-            string *stringCheck = processString(deconstructedQuery[i]);
-        }
-
-        cout << "Token: " << deconstructedQuery[i] << endl;
     }
 
     switch (tableSelected)
     {
     case 1:
-        // employees.insert(pair<int, Employee>(100, Employee(100, 10, "Allan", "Lopez", "Manager")));
-        cout << "Created Employee! with: " << _values << endl;
-        break;
+    {
+        if (deconstructedQuery.size() == 5)
+        {
+            int id = processInt(deconstructedQuery[0]);
+            int departmentId = processInt(deconstructedQuery[1]);
+            string *firstName = processString(deconstructedQuery[2]);
+            string *lastName = processString(deconstructedQuery[3]);
+            string *jobTitle = processString(deconstructedQuery[4]);
+
+            employees.insert(pair<int, Employee>(id, Employee(id, departmentId, *firstName, *lastName, *jobTitle)));
+
+            for (const auto &entry : employees)
+            {
+                cout << "Employee ID: " << entry.first << ", First Name: " << entry.second.getFirstName() << endl;
+            }
+
+            cout << "Created Employee!" << _values << endl;
+        }
+        else
+        {
+            cout << "DID NOT Created Employee!" << _values << endl;
+        }
+    }
+    break;
+
     case 2:
-        cout << "Created Department!" << _values << endl;
-        break;
+    {
+        if (deconstructedQuery.size() == 5)
+        {
+            int id = processInt(deconstructedQuery[0]);
+            int companyId = processInt(deconstructedQuery[1]);
+            int managedBy = processInt(deconstructedQuery[2]);
+            string *name = processString(deconstructedQuery[3]);
+            string *location = processString(deconstructedQuery[4]);
+
+            departments.insert(pair<int, Department>(id, Department(id, companyId, managedBy, *name, *location)));
+
+            for (const auto &entry : departments)
+            {
+                cout << "Department ID: " << entry.first << ", Name: " << entry.second.getManagedBy() << endl;
+            }
+
+            cout << "Created Department!" << _values << endl;
+        }
+        else
+        {
+            cout << "DID NOT Created Department!" << _values << endl;
+        }
+    }
+
+    break;
+
     case 3:
-        cout << "Created Company!" << _values << endl;
-        break;
+    {
+        if (deconstructedQuery.size() == 2)
+        {
+            int id = processInt(deconstructedQuery[0]);
+            string *name = processString(deconstructedQuery[1]);
+
+            companies.insert(pair<int, Company>(id, Company(id, *name)));
+            for (const auto &entry : companies)
+            {
+                cout << "Company ID: " << entry.first << ", Name: " << entry.second.getName() << endl;
+            }
+            cout << "Created Company!" << _values << endl;
+        }
+        else
+        {
+            cout << "DID NOT Created Company!" << _values << endl;
+        }
+    }
+    break;
+
     default:
         cout << "I don't even know how you got here!" << endl;
         exit(0);
@@ -277,12 +345,6 @@ void processSQL(string &_sqlQuery)
         deconstructedQuery.push_back(query);
     }
 
-    // print whats in the dQ
-    for (const std::string &q : deconstructedQuery)
-    {
-        std::cout << q << std::endl;
-    }
-
     // Format operator to be capitalized
     string *uppercaseOperator = toUppercase(deconstructedQuery[0]);
 
@@ -290,7 +352,6 @@ void processSQL(string &_sqlQuery)
     size_t fromPosition = std::string::npos;
     for (size_t i = 0; i < deconstructedQuery.size(); ++i)
     {
-        cout << "checking: " << deconstructedQuery[i] << endl;
         if (deconstructedQuery[i] == "from")
         {
             fromPosition = i;
@@ -405,38 +466,34 @@ int main()
 
     cout << "\nDescription: Design a simple command-line database where you use pointers to manage records, tables, and data relationships. You can implement basic database operations like insertion, retrieval, and deletion.\n";
 
-    // Initialize tables
-    map<int, Employee> employees;
-    map<int, Department> departments;
-    map<int, Company> companies;
-
     // Populate tables
-    companies.insert(pair<int, Company>(1, Company(1, "Apple")));
-    companies.insert(pair<int, Company>(2, Company(2, "Microsoft")));
+    companies.insert(pair<int, Company>(1000, Company(1000, "Apple")));
+    companies.insert(pair<int, Company>(1001, Company(1001, "Microsoft")));
 
-    departments.insert(pair<int, Department>(10, Department(10, 1, 100, "Finance", "New York")));
-    departments.insert(pair<int, Department>(11, Department(11, 1, 101, "Engineering", "New York")));
-    departments.insert(pair<int, Department>(12, Department(12, 1, 102, "Design", "Los Angeles")));
-    departments.insert(pair<int, Department>(13, Department(13, 2, 103, "Design", "Montreal")));
-    departments.insert(pair<int, Department>(14, Department(14, 2, 104, "Engineering", "New York")));
+    departments.insert(pair<int, Department>(2000, Department(2000, 1000, 3000, "Finance", "New York")));
+    departments.insert(pair<int, Department>(2001, Department(2001, 1000, 3001, "Engineering", "New York")));
+    departments.insert(pair<int, Department>(2002, Department(2002, 1000, 3002, "Design", "Los Angeles")));
+    departments.insert(pair<int, Department>(2003, Department(2003, 1001, 3003, "Design", "Montreal")));
+    departments.insert(pair<int, Department>(2004, Department(2004, 1001, 3004, "Engineering", "New York")));
 
-    employees.insert(pair<int, Employee>(100, Employee(100, 10, "Allan", "Lopez", "Manager")));
-    employees.insert(pair<int, Employee>(101, Employee(101, 11, "Jacob", "Smith", "Manager")));
-    employees.insert(pair<int, Employee>(102, Employee(102, 12, "Elena", "Stroop", "Manager")));
-    employees.insert(pair<int, Employee>(103, Employee(103, 13, "Victor", "Kiltz", "Manager")));
-    employees.insert(pair<int, Employee>(104, Employee(104, 14, "Ting", "Chen", "Manager")));
-    employees.insert(pair<int, Employee>(105, Employee(105, 12, "Justine", "Khan", "Designer")));
-    employees.insert(pair<int, Employee>(106, Employee(106, 10, "Edgar", "Mohamed", "Finance")));
-    employees.insert(pair<int, Employee>(107, Employee(107, 10, "Fiona", "Appleton", "Finance")));
-    employees.insert(pair<int, Employee>(108, Employee(108, 11, "Manon", "Messier", "Engineer")));
-    employees.insert(pair<int, Employee>(109, Employee(109, 13, "Katelyn", "Ronston", "Designer")));
-    employees.insert(pair<int, Employee>(110, Employee(110, 14, "Paul", "Patel", "Engineer")));
-    employees.insert(pair<int, Employee>(111, Employee(111, 11, "Lucas", "Strong", "Engineer")));
+    employees.insert(pair<int, Employee>(3000, Employee(3000, 2000, "Allan", "Lopez", "Manager")));
+    employees.insert(pair<int, Employee>(3001, Employee(3001, 2001, "Jacob", "Smith", "Manager")));
+    employees.insert(pair<int, Employee>(3002, Employee(3002, 2002, "Elena", "Stroop", "Manager")));
+    employees.insert(pair<int, Employee>(3003, Employee(3003, 2003, "Victor", "Kiltz", "Manager")));
+    employees.insert(pair<int, Employee>(3004, Employee(3004, 2004, "Ting", "Chen", "Manager")));
+    employees.insert(pair<int, Employee>(3005, Employee(3005, 2002, "Justine", "Khan", "Designer")));
+    employees.insert(pair<int, Employee>(3006, Employee(3006, 2000, "Edgar", "Mohamed", "Finance")));
+    employees.insert(pair<int, Employee>(3007, Employee(3007, 2000, "Fiona", "Appleton", "Finance")));
+    employees.insert(pair<int, Employee>(3008, Employee(3008, 2001, "Manon", "Messier", "Engineer")));
+    employees.insert(pair<int, Employee>(3009, Employee(3009, 2003, "Katelyn", "Ronston", "Designer")));
+    employees.insert(pair<int, Employee>(3010, Employee(3010, 2004, "Paul", "Patel", "Engineer")));
+    employees.insert(pair<int, Employee>(3011, Employee(3011, 2001, "Lucas", "Strong", "Engineer")));
 
     // for (const auto &entry : employees)
     // {
     //     cout << "Employee ID: " << entry.first << ", Name: " << entry.second.getFirstName() << endl;
     // }
+
     // for (const auto &entry : departments)
     // {
     //     cout << "Department ID: " << entry.first << ", Managed By: " << entry.second.getManagedBy() << endl;
